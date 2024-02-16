3ff581c67f623874b93985261a7eeb5eed8540fb
Dan <iam@dancielos.com>
2024-02-16 14:21:25 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+  Plugin Name: Fry It Custom Plugin
+  Description: Custom blocks for fry it..
+  Version: 1.0
+  Author: Dan Cielos
+  Author URI: https://dancielos.com/
+*/
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class FryItPlugin {
+  function __construct() {
+    add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+  }
+
+  function adminAssets() {
+    wp_enqueue_script('newblock', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks'));
+  }
+}
+
+$fryItPlugin = new FryItPlugin();
